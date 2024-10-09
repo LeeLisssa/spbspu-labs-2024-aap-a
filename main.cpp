9a8caa9a983e480f389f1fd0a2facb242e69e465
lee <likachich12@gmail.com>
2024-10-09 22:02:46 +0300

    Int reading retry

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+	int a = 0;
+	std::cin >> a;
+	if (std::cin.good()) {
+		std::cerr << "good!\n";
+	}
+	else if (std::cin.eof()) {
+		std::cerr << "eof!\n";
+	}
+	else if (std::cin.bad()) {
+		std::cerr << "bad!\n";
+	}
+	else if (std::cin.fail()) {
+		std::cerr << "fail!\n";
+		std::cin.clear();
+		std::cin.ignore(1);
+		std::cin >> a;
+	}
+	std::cout << a;
+	std::cout << "\n";
+}
