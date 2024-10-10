45c42bbe96074e26feebe9b1570078282e0f4c15
lee <likachich12@gmail.com>
2024-10-10 21:12:29 +0300

    remove good check

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@ int main()
 {
 	int a = 0;
 	std::cin >> a;
-	if (std::cin.good()) {
-		std::cerr << "good!\n";
-	}
-	else if (std::cin.eof()) {
+	if (std::cin.eof()) {
 		std::cerr << "eof!\n";
 	}
 	else if (std::cin.bad()) {
